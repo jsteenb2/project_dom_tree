b5a020269e97ad73c4b8a5b96a3231862f81a1a3
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-15 15:31:16 -0700

    added parsing class and struct

--- parser.rb
+++ parser.rb
@@ -1,69 +1,40 @@
 #parser.rb
 require 'ostruct'
 
-tag = OpenStruct.new
-#tag = Struct.new('tag', :attributes)
+Tag = Struct.new(:type, :attributes, :children)
 
+class HTMLParser
+
+  def initialize(html_text)
+    @html = html_text
+    @dom = []
+  end
 
   def parse_tag(html)
     attr_hash = {}
     tag = {:attributes => nil, :tag_type => nil}
     tag[:tag_type] = html.match(/<(\w*)/).captures[0]
-    # tag
     regex = /[a-z]+\s*='[\w\s]*'/
     elements = html.scan(regex)
     elements.each do |info|
       clazz = info.match(/([a-z]*)=/).captures[0]
       clazz_val = info.match(/'(.*)'/).captures[0]
-
       attr_hash[clazz] = clazz_val
     end
     tag[:attributes] = attr_hash
     # puts "hash is #{attr_hash}"
-    p tag
+    # p tag
+    Tag.new(tag[:tag_type], tag[:attributes])
   end
 
- def parse_tagE(html)
-    puts "-----------------------------------------------------------"
-    elements= html.scan(/[a-z]+\s*='[\w\s]*'/)
+  def attr_adder(attributes)
 
-    puts "element is:: #{elements} \n"
-
-    hash = {}
-    hash[:tag] =html.match(/<(\w*)/).captures[0]
-  
-    elements.each do |info|
-      clazz = info.match(/([a-z]*)=/).captures[0].to_sym
-      clazz_val = info.match(/'(.*)'/).captures[0]
-
-      hash[clazz] = clazz_val
-    end
-
-    hash
-
-    print "hash is:: #{hash} \n"
-    puts "-----------------------------------------------------------"
   end
 
-a_hash = parse_tagE("<p class='foo bar' type='' id='baz' name='fozzie'>")
-puts a_hash
-#every key in the hash, assign it to tag"
+end
 
-# a_hash.each_key do |key|
-#   tag.key = a_hash[key]
-# end
+html_string = "<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>"
 
-# print tag
-# puts tag.class
-# puts tag.id
-
-
-
-
-
-
-
-
-
-
-#print tag.name = "h[:attributes][0]"
+# a_hash = parse_tag("<p class='foo bar' type='' id='baz' name='fozzie'>")
+p HTMLParser.new(html_string).parse_tag(html_string)
+p HTMLParser.new(html_string).parse_tag(html_string).attributes
