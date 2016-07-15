1707ce31779bbe41695a0691840a9f82126d9f63
Adrian Mui <adrian.mui@gmail.com>
2016-07-15 17:45:53 -0400

    go

--- parser.rb
+++ parser.rb
@@ -1,15 +1,9 @@
 #parser.rb
+require 'ostruct'
 
+tag = OpenStruct.new
+#tag = Struct.new('tag', :attributes)
 
-
-  #parses string into hash/struct containing key attributes using Regex
-
-  # "<p class='foo bar' id='baz' name='fozzie'>"
-
-  # <p
-  # class='foo bar'
-  # id='baz'
-  # name='fozzie'>
 
   def parse_tag(html)
     attr_hash = {}
@@ -29,4 +23,47 @@
     p tag
   end
 
-  parse_tag("<p class='foo bar' type='' id='baz' name='fozzie'>")
+ def parse_tagE(html)
+    puts "-----------------------------------------------------------"
+    elements= html.scan(/[a-z]+\s*='[\w\s]*'/)
+
+    puts "element is:: #{elements} \n"
+
+    hash = {}
+    hash[:tag] =html.match(/<(\w*)/).captures[0]
+  
+    elements.each do |info|
+      clazz = info.match(/([a-z]*)=/).captures[0].to_sym
+      clazz_val = info.match(/'(.*)'/).captures[0]
+
+      hash[clazz] = clazz_val
+    end
+
+    hash
+
+    print "hash is:: #{hash} \n"
+    puts "-----------------------------------------------------------"
+  end
+
+a_hash = parse_tagE("<p class='foo bar' type='' id='baz' name='fozzie'>")
+puts a_hash
+#every key in the hash, assign it to tag"
+
+# a_hash.each_key do |key|
+#   tag.key = a_hash[key]
+# end
+
+# print tag
+# puts tag.class
+# puts tag.id
+
+
+
+
+
+
+
+
+
+
+#print tag.name = "h[:attributes][0]"
